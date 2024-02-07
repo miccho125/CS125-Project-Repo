c2d57fa9a0d6ae2046f16ffcea05b76943110410
Riley Faiferlick <rileyman324@gmail.com>
2024-02-06 22:50:15 -0800

    Worked out basic saving functionality. (See MainActivity.kt and activity_main.xml for most of code)
    Note: only saves when manually closing app on android phone, not when pressing red stop app button

--- app/src/main/java/com/example/cs125app/MainActivity.kt
+++ app/src/main/java/com/example/cs125app/MainActivity.kt
@@ -1,7 +1,13 @@
 package com.example.cs125app
 
 import android.os.Bundle
+import android.content.SharedPreferences
 import androidx.activity.ComponentActivity
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
@@ -12,35 +18,31 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.cs125app.ui.theme.CS125AppTheme
 
-class MainActivity : ComponentActivity() {
+public class MainActivity : ComponentActivity() {
+
+    lateinit var username: EditText;
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            CS125AppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("CS 125 Testing")
-                }
-            }
-        }
+        setContentView(R.layout.activity_main)
+
+        username = findViewById(R.id.EditUserName)
+
+        val settings = getSharedPreferences("UserInfo", 0)
+        val n = settings.getString("username","")
+        username.setText(n)
     }
+
+    override fun onStop()
+    {
+        super.onStop()
+        val settings = getSharedPreferences("UserInfo", 0)
+        val edit = settings.edit()
+
+        edit.putString("username",username.text.toString())
+        edit.apply()
+
+    }
+
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CS125AppTheme {
-        Greeting("CS 125 Testing")
-    }
-}
